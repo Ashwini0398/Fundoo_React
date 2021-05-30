e8dcd8861aa5c659f9df7d7b530eedb8eeaedc24
Ashwini0398 <ashwinikhonde03@gmail.com>
2021-05-30 23:36:08 +0530

    ADDED: fundoo reg from logo and tag designed

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import './Registration.scss';
 
 export default class Registration extends Component {
     render() {
         return (
-            <div>
-                hello
-            </div>
+            <>
+                <div className="reg-frame">
+                    <div className="reg-cont">
+                    <div className="reg-fundoo">
+                        <span className="f">F</span>
+                        <span className="u">u</span>
+                        <span className="n">n</span>
+                        <span className="d">d</span>
+                        <span className="o">o</span>
+                        <span className="u">o</span>
+                    </div>
+                    <div className="reg-fundoo-account">
+                        Create Your Fundoo Account
+                    </div>
+                    </div>
+                    <div className="logo-cont">logo</div>
+                </div>
+            </>
         );
     }
 }
