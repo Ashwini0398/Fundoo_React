a26bfbf641ae72b22a9f4ebe400adb5bac6185bb
Ashwini0398 <ashwinikhonde03@gmail.com>
2021-05-31 13:02:07 +0530

    MODIFIED: username input part added

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
 import './Registration.scss';
+import Checkbox from '@material-ui/core/Checkbox';
 
 export default class Registration extends Component {
     render() {
@@ -7,21 +9,37 @@ export default class Registration extends Component {
             <>
                 <div className="reg-frame">
                     <div className="reg-cont">
-                    <div className="reg-fundoo">
-                        <span className="f">F</span>
-                        <span className="u">u</span>
-                        <span className="n">n</span>
-                        <span className="d">d</span>
-                        <span className="o">o</span>
-                        <span className="u">o</span>
+                        <div className="reg-fundoo">
+                            <span className="f">F</span>
+                            <span className="u">u</span>
+                            <span className="n">n</span>
+                            <span className="d">d</span>
+                            <span className="o">o</span>
+                            <span className="u">o</span>
+                        </div>
+                        <div className="reg-fundoo-account">
+                            Create Your Fundoo Account
                     </div>
-                    <div className="reg-fundoo-account">
-                        Create Your Fundoo Account
+                        <div className="reg-content-names">
+                            <TextField id="firstName" label="FirstName" variant="outlined" size="small" />
+                            <TextField id="lastName" label="LastName" variant="outlined" size="small" />
+                        </div>
+                        <div className="reg-content-username">
+                            <TextField id="username" label="UserName" variant="outlined" size="small" />
+                            <span>You can use letters,numbers & periods</span>
+                        </div>
+                        <div className="reg-content-security">
+                            <TextField id="password" label="Password" variant="outlined" size="small" />
+                            <TextField id="Confirm" label="Confirm Password" variant="outlined" size="small" />
+                        </div>
+                        <span className="message">Use 8 or more characters with a mix of letters, number & symbols</span>
                     </div>
+                    <div>
+                        <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
                     </div>
                     <div className="logo-cont">
-                    <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="" />
-                    <span class="fig-caption">One account. All of Google working for you.</span>
+                        <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="" />
+                        <span class="fig-caption">One account. All of Google working for you.</span>
                     </div>
                 </div>
             </>
